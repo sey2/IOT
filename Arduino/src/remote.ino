1810ba92c6dfc24399262002a51279ca47d39cc0
sey2 <54762273+sey2@users.noreply.github.com>
2022-03-31 09:02:51 +0900

    [Update] 과제 코드 리팩토링 (03.31)

--- Arduino/src/remote.ino
+++ Arduino/src/remote.ino
@@ -4,6 +4,7 @@
 #include "PinDefinitionsAndMore.h"
 
 int ledPin = 9;
+
 void setup() {
     Serial.begin(9600);
     
@@ -17,7 +18,12 @@ void setup() {
 
 int brightness = 0;
 
-int toneArray[] = {523, 587, 659, 698};
+/* 도(1), 레(2), 미(3), 파(4), 솔(5), 라(6), 시(7), 도(8)*/
+int toneArray[] = {262, 294, 330, 349, 392, 440, 494, 523};
+
+/* 1 ~ 8 번 버튼을 눌렀을 때 들어오는 데이터를 담은 배열 */
+uint32_t remoteDataArr[] = {4077715200, 3877175040, 2707357440, 4144561920};
+
 void loop() {
     if (IrReceiver.decode()) {
         Serial.println();
@@ -36,26 +42,35 @@ void loop() {
               ledBrightness(brightness);
               Serial.println("Pressed - Button");
         }
-        else if (myRawData == 4077715200) { /*  도 (1) */
-              playTone(523);
+        else{
+              playTone(myRawData);
+        }
+     
+        /*
+        else if (myRawData == 4077715200) { 
+              playTone(262);
               Serial.println("Pressed 1 Button");
         }
-        else if (myRawData == 3877175040) { /* 레(2) */        
-              playTone(587);
+        else if (myRawData == 3877175040) {       
+              playTone(294);
               Serial.println("Pressed 2 Button");
         }
-        else if (myRawData == 2707357440) { /* 미(3) */        
-              playTone(659);    
+        else if (myRawData == 2707357440) {       
+              playTone(330);    
               Serial.println("Pressed 3 Button");
         }
-        else if (myRawData == 4144561920) { /* 파(4) */              
-              playTone(698);
+        else if (myRawData == 4144561920) {               
+              playTone(349);
               Serial.println("Pressed 4 Button"); 
         }
+        */
+
+          
         IrReceiver.resume(); // receive the next value
        
     }      delay(30);
 }
+
 
 void ledBrightness(int brightness){
    IrReceiver.stop();
@@ -63,9 +78,24 @@ void ledBrightness(int brightness){
    IrReceiver.start(8000);  
 }
 
+/*
+
 void playTone(int toneData){
    IrReceiver.stop();    // 멈추고 다시 시작을 안해주면 처음 누를 땐 정확한 값이 나오는데 그렇지 않으면 계속 쓰레기 값 나옴
    tone(TONE_PIN, toneData, 500);  
    IrReceiver.start(8000);
 }
+*/
 
+
+void playTone(int myRawData){
+   for(int i=0; i=6; i++){
+      if(myRawData == remoteDataArr[i]){
+        IrReceiver.stop();    // 멈추고 다시 시작을 안해주면 처음 누를 땐 정확한 값이 나오는데 그렇지 않으면 계속 쓰레기 값 나옴
+        tone(TONE_PIN, toneArray[i], 500);
+        IrReceiver.start(8000);
+        Serial.println((String)"Pressed " + i+1 + (String)" Button");
+        return;
+      }  
+   }
+}
