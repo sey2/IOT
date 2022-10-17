7647f0e099bb05101bc0809c47b3e7e96a010b1b
sey2 <54762273+sey2@users.noreply.github.com>
2022-10-17 18:56:10 +0900

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,5 +1,6 @@
 package org.techtown.myapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,6 +8,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,9 +21,10 @@ public class MainActivity extends AppCompatActivity {
         chosunBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), ChosunActivity.class);
-                //Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www3.chosun.ac.kr/chosun/index.do"));
-                startActivity(intent);
+                //Intent intent = new Intent(getApplicationContext(), ChosunActivity.class);
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www3.chosun.ac.kr/chosun/index.do"));
+                 startActivity(intent);
+                startActivityForResult(intent, 101);
             }
         });
 
@@ -43,4 +46,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    /*
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        
+        if(requestCode == 101){
+            Toast.makeText(getApplicationContext(), "OnActivityResult 호출" + requestCode + ", 결과 코드 : " + 
+                    resultCode, Toast.LENGTH_LONG);
+            
+            if(resultCode == RESULT_OK){
+                String name = data.getStringExtra("name");
+                Toast.makeText(getApplicationContext(), "응답으로 전달된 name : " + name, Toast.LENGTH_LONG).show();
+            }
+                    
+        }
+    }
+    
+     */
 }
